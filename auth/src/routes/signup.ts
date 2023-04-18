63698055872740417441672fbd371dd6adcd3686
Wenjun Zeng <wz165@duke.edu>
2023-04-17 23:55:27 -0400

    finishing auth

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,11 @@
 import express, {Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
 
-import { body, validationResult } from 'express-validator';
-import {RequestValidationError} from "../errors/request-validation-error";
+import { body } from 'express-validator';
 import {BadRequestError} from "../errors/bad-request-error";
 import {DatabaseConnectionError} from "../errors/database-connection-error";
 import {User} from "../models/user";
+import {validateRequest} from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -17,12 +17,9 @@ router.post('/api/users/signup', [body('email')
         .isLength({ min: 4, max: 20 })
         .withMessage("Password must be between 4 and 20 characters!")
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+    //validateRequest
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -53,7 +50,6 @@ router.post('/api/users/signup', [body('email')
     res.status(201).send(user);
 
 });
-
 
 
 export { router as signupRouter };
