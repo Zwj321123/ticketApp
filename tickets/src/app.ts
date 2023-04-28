a7825984d10da882d538c87d48268272cdae3eda
Wenjun Zeng <wz165@duke.edu>
2023-04-28 01:09:41 -0400

    setup tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import {errorHandler} from '@zwjtickets/common';
+import {NotFoundError} from "@zwjtickets/common";
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}));
+
+app.all('*', async (req, res, next) => {
+    throw (new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export default app;
